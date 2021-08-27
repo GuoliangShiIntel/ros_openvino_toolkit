565c331f1b62f917b365db8aac6e14dddef13545
GuoliangShi <1342722738@qq.com>
2021-08-27 09:24:00 +0800

    update object_msgs

--- object_msgs/obj-x86_64-linux-gnu/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/sgl/Github/catkin_wa/src/OV_test/object_msgs/obj-x86_64-linux-gnu/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
